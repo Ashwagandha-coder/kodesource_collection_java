0feb05fd3ab5fa8799e926f73f023704f8f1657b
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-02 12:12:40 +0500

    ready six task

---
+++ src/ArrayList/Six.java
@@ -0,0 +1,35 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class Six {
+
+
+    public static void main(String[] args) {
+
+        new RemoveElementArrayList().TestRemoveElementArrayList();
+
+    }
+}
+
+class RemoveElementArrayList {
+
+    public void TestRemoveElementArrayList() {
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("Fork");
+        arrayList.add("Spoon");
+        arrayList.add("Knife");
+        arrayList.add("Plate");
+        arrayList.add("Ice");
+
+        arrayList.remove(2);
+
+        System.out.println(arrayList);
+
+
+    }
+
+
+}
