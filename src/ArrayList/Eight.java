fdf31dcf38898cc6ff85d82071047a5a56b21676
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-02 12:26:20 +0500

    ready Eight.java task

---
+++ src/ArrayList/Eight.java
@@ -0,0 +1,41 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Eight {
+
+
+    public static void main(String[] args) {
+
+        new SortArrayList().TestSortArrayList();
+
+    }
+}
+
+class SortArrayList {
+
+    public void TestSortArrayList() {
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        arrayList.add(100);
+        arrayList.add(206);
+        arrayList.add(530);
+        arrayList.add(40);
+        arrayList.add(510);
+        arrayList.add(960);
+
+        Collections.sort(arrayList);
+
+        System.out.println(arrayList);
+
+
+
+
+
+    }
+
+
+
+}
