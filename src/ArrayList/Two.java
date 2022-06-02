e8b5495e746895eaad93c54eec80cb57c4f9457f
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-02 11:47:40 +0500

    ready two task

---
+++ src/ArrayList/Two.java
@@ -0,0 +1,40 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class Two {
+
+
+    public static void main(String[] args) {
+
+        new ItterativeArrayList().TestItterativeArrayList();
+
+    }
+}
+
+class ItterativeArrayList {
+
+    public void TestItterativeArrayList() {
+
+        ArrayList<String> arrayList = new ArrayList<>(10);
+
+        arrayList.add("Apple");
+        arrayList.add("Orange");
+        arrayList.add("Banana");
+        arrayList.add("Pear");
+        arrayList.add("Appricot");
+        arrayList.add("Water Melon");
+
+        for (String str : arrayList) {
+
+            System.out.println(str);
+
+        }
+
+
+
+
+    }
+
+
+}
