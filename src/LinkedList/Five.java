896092b866bbbb8620336a3eb4902ec494734617
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-14 13:45:52 +0500

    ready task five LinkedList

--- src/LinkedList/Five.java
+++ src/LinkedList/Five.java
@@ -1,4 +1,35 @@
 package LinkedList;
 
+import java.util.LinkedList;
+
 public class Five {
+
+
+    public static void main(String[] args) {
+
+        new AddLinkedListElem().addElem();
+
+    }
+}
+
+class AddLinkedListElem {
+
+    public void addElem() {
+
+        LinkedList<String> list = new LinkedList<>();
+
+        list.add("Masha");
+        list.add("Natasha");
+        list.add("Dasha");
+        list.add("Olya");
+        list.add("Nika");
+        list.add("Lena");
+
+        list.add(3,"Mike");
+
+        System.out.println(list);
+
+
+    }
+
 }
