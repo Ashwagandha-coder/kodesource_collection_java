371ba55509b8303c96c91a711a224ba213d2d621
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-14 14:10:13 +0500

    ready task ten LinkedList

--- src/LinkedList/Ten.java
+++ src/LinkedList/Ten.java
@@ -1,4 +1,34 @@
 package LinkedList;
 
+import java.util.LinkedList;
+
 public class Ten {
+
+
+    public static void main(String[] args) {
+
+        new GetElemLinkedList().getElem();
+
+    }
+}
+
+class GetElemLinkedList {
+
+    public void getElem() {
+
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+
+        list.add(60);
+
+        System.out.println(list.get(0) + " " + list.get(list.size() - 1));
+
+
+    }
+
+
 }
