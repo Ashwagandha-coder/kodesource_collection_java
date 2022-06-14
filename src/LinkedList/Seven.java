a770ddf7299ea392bdda3f8c049bce69a07860e8
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-14 13:53:42 +0500

    ready task seven LinkedList

--- src/LinkedList/Seven.java
+++ src/LinkedList/Seven.java
@@ -1,4 +1,36 @@
 package LinkedList;
 
+import java.util.LinkedList;
+
 public class Seven {
+
+
+    public static void main(String[] args) {
+
+        new BeginAddElemInLinkedList().addBeginElem();
+
+    }
+}
+
+class BeginAddElemInLinkedList {
+
+    public void addBeginElem() {
+
+        LinkedList<String> list = new LinkedList<>();
+
+
+        list.add("Popa");
+        list.add("Dopa");
+        list.add("Nopa");
+        list.add("Jopa");
+        list.add("Goba");
+
+        list.add(0,"Roja");
+
+        System.out.println(list);
+
+
+    }
+
+
 }
