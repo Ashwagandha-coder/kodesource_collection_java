22de43d502f2c4ff2598227255518914a1f63302
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-14 13:49:14 +0500

    ready task six LinkedList

--- src/LinkedList/Six.java
+++ src/LinkedList/Six.java
@@ -1,4 +1,32 @@
 package LinkedList;
 
+import java.util.LinkedList;
+
 public class Six {
+
+
+    public static void main(String[] args) {
+
+        new AddElemInLinkedList().addElement();
+
+    }
+}
+
+class AddElemInLinkedList {
+
+    private LinkedList<Integer> list;
+
+    public void addElement() {
+
+        list = new LinkedList<>();
+
+        list.add(279);
+
+        System.out.println(list);
+
+
+
+    }
+
+
 }
