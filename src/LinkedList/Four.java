0a819162e451b36cd4d099828da7a05f156ecd52
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-14 13:35:02 +0500

    ready task four LinkedList

--- src/LinkedList/Four.java
+++ src/LinkedList/Four.java
@@ -1,4 +1,38 @@
 package LinkedList;
 
+import java.util.LinkedList;
+
 public class Four {
+
+
+    public static void main(String[] args) {
+
+        new ItterationLinkedList().itterateLinkedList();
+
+    }
+}
+
+class ItterationLinkedList {
+
+    public void itterateLinkedList() {
+
+        LinkedList<Integer> list = new LinkedList<>();
+
+
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.add(60);
+
+        for (int i = list.size() - 1; i >= 0; i--) {
+
+            System.out.println(list.get(i));
+
+        }
+
+
+    }
+
+
 }
