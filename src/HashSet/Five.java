796354ff33b03bd1054c74fc4d0d5d0ae1b20954
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-15 15:22:12 +0500

    five task ready HashSet

--- src/HashSet/Five.java
+++ src/HashSet/Five.java
@@ -1,4 +1,34 @@
 package HashSet;
 
+import java.util.HashSet;
+
 public class Five {
+
+
+    public static void main(String[] args) {
+
+        new EmptyHashSet().emptyElemHashSet();
+
+    }
+}
+
+class EmptyHashSet {
+
+    public void emptyElemHashSet() {
+
+        HashSet<String> hashSet = new HashSet<>();
+
+
+        hashSet.add("fbdfb");
+        hashSet.add("rwefvxc");
+        hashSet.add("sfeewf");
+        hashSet.add("wqdfdvcvv");
+
+        System.out.println(hashSet.isEmpty());
+
+
+
+    }
+
+
 }
