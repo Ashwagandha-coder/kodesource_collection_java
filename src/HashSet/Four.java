b5c7cdf3bf30fa368c4bc73eb2dc2c9c5199a33e
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-15 15:17:50 +0500

    four task ready HashSet

--- src/HashSet/Four.java
+++ src/HashSet/Four.java
@@ -1,4 +1,36 @@
 package HashSet;
 
+import java.util.HashSet;
+
 public class Four {
+
+
+    public static void main(String[] args) {
+
+        new ClearHashSet().clearElemHashSet();
+
+    }
+}
+
+class ClearHashSet {
+
+    public void clearElemHashSet() {
+
+        HashSet<Integer> hashSet = new HashSet<>();
+
+
+        hashSet.add(222);
+        hashSet.add(333);
+        hashSet.add(444);
+        hashSet.add(555);
+
+        hashSet.clear();
+
+        System.out.println(hashSet);
+
+
+
+    }
+
+
 }
