2fbf04f1fbfcd84aedf5280e774a60b8d76f5d12
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-16 13:37:42 +0500

    ready three task TreeSet

--- src/Collections/TreeSet/Three.java
+++ src/Collections/TreeSet/Three.java
@@ -1,4 +1,39 @@
 package Collections.TreeSet;
 
+import java.util.TreeSet;
+
 public class Three {
+
+
+    public static void main(String[] args) {
+
+        new NewTreeSet().copyTreeSet();
+
+    }
+}
+
+class NewTreeSet {
+
+    public void copyTreeSet() {
+
+        TreeSet<Integer> treeSet = new TreeSet<>();
+
+        treeSet.add(1);
+        treeSet.add(2);
+        treeSet.add(3);
+        treeSet.add(4);
+        treeSet.add(5);
+        treeSet.add(6);
+
+        TreeSet<Integer> treeSet2 = new TreeSet<>(treeSet);
+
+
+        System.out.println(treeSet2);
+
+
+
+    }
+
+
+
 }
