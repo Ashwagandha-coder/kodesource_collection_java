4b8d55b0b2dc8cd59a1b97ce1d25a64d57181ceb
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-16 13:29:53 +0500

    ready two task TreeSet

--- src/Collections/TreeSet/Two.java
+++ src/Collections/TreeSet/Two.java
@@ -1,4 +1,35 @@
 package Collections.TreeSet;
 
+import java.util.TreeSet;
+
 public class Two {
+
+
+    public static void main(String[] args) {
+
+        new ForEachTreeSet().forEachElemTreeSet();
+
+    }
+}
+
+class ForEachTreeSet {
+
+    public void forEachElemTreeSet() {
+
+        TreeSet<String> treeSet = new TreeSet<>();
+
+
+        treeSet.add("Join");
+        treeSet.add("Return");
+        treeSet.add("For");
+        treeSet.add("Interface");
+        treeSet.add("Class");
+
+        treeSet.forEach(e -> System.out.println(e));
+
+
+
+    }
+
+
 }
