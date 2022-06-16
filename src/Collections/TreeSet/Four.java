1076354b13a0087436ab725f5f1c7cc29378c4d4
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-16 13:44:27 +0500

    ready four task TreeSet

--- src/Collections/TreeSet/Four.java
+++ src/Collections/TreeSet/Four.java
@@ -1,4 +1,37 @@
 package Collections.TreeSet;
 
+import java.util.TreeSet;
+
 public class Four {
+
+
+    public static void main(String[] args) {
+
+        new ViceVersaTreeSet().ForViceVersaTreeSet();
+
+    }
+}
+
+class ViceVersaTreeSet {
+
+    public void ForViceVersaTreeSet() {
+
+        TreeSet<Integer> treeSet = new TreeSet<>();
+
+        treeSet.add(1);
+        treeSet.add(2);
+        treeSet.add(3);
+        treeSet.add(4);
+        treeSet.add(5);
+        treeSet.add(6);
+
+
+        System.out.println(treeSet.descendingSet());
+
+
+
+    }
+
+
+
 }
