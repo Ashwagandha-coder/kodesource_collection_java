ef5ea31327d23a97b8a2268198b4d3bd4fdb7a12
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-19 13:33:45 +0500

    ready task three PriorityQueue

--- src/Collections/PriorityQueue/Three.java
+++ src/Collections/PriorityQueue/Three.java
@@ -1,4 +1,36 @@
 package Collections.PriorityQueue;
 
+import java.util.PriorityQueue;
+
 public class Three {
+
+
+    public static void main(String[] args) {
+
+        new CopyPriorityQueue().copyElemPriorityQueue();
+
+    }
 }
+
+class CopyPriorityQueue {
+
+    public void copyElemPriorityQueue() {
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        priorityQueue.offer(13);
+        priorityQueue.offer(22);
+        priorityQueue.offer(33);
+        priorityQueue.offer(11);
+
+        PriorityQueue<Integer> priorityQueue2 = new PriorityQueue<>(priorityQueue);
+
+        System.out.println(priorityQueue2);
+
+
+    }
+
+
+}
+
+// при выводе на консоль не сохранятся приоритет
