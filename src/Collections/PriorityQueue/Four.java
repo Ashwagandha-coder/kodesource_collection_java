cacbc432a0474d04494cd68f3ac35b23e1e7c0e3
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-19 13:39:40 +0500

    ready task four PriorityQueue

--- src/Collections/PriorityQueue/Four.java
+++ src/Collections/PriorityQueue/Four.java
@@ -1,4 +1,37 @@
 package Collections.PriorityQueue;
 
+import java.util.PriorityQueue;
+
 public class Four {
+
+
+    public static void main(String[] args) {
+
+        new AddPriorityQueue3().addElemPriorityQueue3();
+
+
+    }
+}
+
+class AddPriorityQueue3 {
+
+    public void addElemPriorityQueue3() {
+
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>();
+
+
+        priorityQueue.offer("vsfdvf");
+        priorityQueue.offer("fdb");
+        priorityQueue.offer("weqwd");
+
+        for (String string : priorityQueue)
+            System.out.println(string);
+
+
+
+
+    }
+
+
+
 }
