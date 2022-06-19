a1a29105250ddf2012798917dca6104dfc5ea703
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-19 12:25:12 +0500

    ready one task PriorityQueue

--- src/Collections/PriorityQueue/One.java
+++ src/Collections/PriorityQueue/One.java
@@ -1,4 +1,35 @@
 package Collections.PriorityQueue;
 
+import java.util.PriorityQueue;
+
 public class One {
+
+    public static void main(String[] args) {
+
+        new AddPriorityQueue().addElemPriorityQueue();
+
+    }
+
+
+}
+
+class AddPriorityQueue {
+
+    public void addElemPriorityQueue() {
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        priorityQueue.add(2);
+        priorityQueue.add(3);
+        priorityQueue.add(4);
+        priorityQueue.add(5);
+
+        System.out.println(priorityQueue);
+
+
+
+    }
+
+
+
 }
